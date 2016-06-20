34a173dcf8ed3f95a1865970d57e5e9ebd02e662
Gengsheng Liu <gsliu.tju@gmail.com>
2016-06-19 19:42:09 -0700

    function complete

--- searchDispatcher/logExtractor/logExtractor.py
+++ searchDispatcher/logExtractor/logExtractor.py
@@ -1,29 +1,33 @@
 import os
 import tarfile
 
+g_after_extract_path='/data/data/bundle_after_extract/'
+g_before_extract_path='/data/data/bundle_before_extract/'
+
 class logExtractor:
     'Base class to extract log bundles of ESXi, VC or other product'
-
-    g_after_extract_path='/data/data/bundle_after_extract/'
-    g_before_extract_path='/data/data/bundle_before_extract/'
-
     def __init__(self, file):
-        self.file=file
-	print file
-        print 'request is a service bundle...'
-        print 'Initing log extractor!'
+        self.file = file
+	print "Input bundle is %s"  %  file
 
 class esxiLogExtractor(logExtractor):
     'for exi, *.tgz'
-    def __init__(self):
+    def __init__(self, file):
         logExtractor.__init__(self, file)
     def extract(self):
+        print 'Initing ESXi extractor...'
         split_filename = self.file.rsplit('.', 1)
         if "tgz" in split_filename:
             fn = self.file.decode('unicode-escape')
             tar = tarfile.open(fn, "r:gz")
             extracted = tar.next().name.split("/")[0]
-            print extracted
+          
+            os.system("umask 000;tar zxvf " + self.file + ' -C ' + g_after_extract_path)
+
+            ret = g_after_extract_path + extracted
+            os.system("cd " + ret  + '/' + '; ./reconstruct.sh')
+#            print "cd " + ret + '/' + '; ./reconstruct.sh'
+            return ret
 
         else:
             print "The service bundle of ESXi host should by *.tgz, please check"
@@ -45,7 +49,8 @@ class urlLogExtractor(logExtractor):
 
 
 if __name__ == '__main__':
-    bundle = "vmvspm0141.nm.nmfco.com-vmsupport-2016-02-23\@15-05-08.tgz"
-    esx = esxiLogExtractor(g_before_extract_path + bundle)
-    esx.extract()
-
+    bundle = "vmvspm0141.nm.nmfco.com-vmsupport-2016-02-23@15-05-08.tgz"
+    bundle_full_path = g_before_extract_path + bundle
+    esx = esxiLogExtractor(bundle_full_path)
+    ret = esx.extract()
+    print ret
