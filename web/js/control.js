0c26c14ab0b1efa1bf27532a6c4899a1020620db
Gengsheng Liu <gliu@gliu.eng.vmware.com>
2016-06-21 18:08:30 +0800

    update UI

--- web/js/control.js
+++ web/js/control.js
@@ -22,8 +22,14 @@ $( document ).ready(function() {
     console.log( "ready!" );
     listtophits();
 });
+var dosearch = false;
 
 function makesearch() {
+    if(dosearch == true) {
+        return;
+    }
+
+    dosearch = true;
 
     $('#div_top_hit').hide();
 
@@ -31,9 +37,13 @@ function makesearch() {
     console.log($('#input_file_upload').get(0).files)
     if($('#input_file_upload').get(0).files.length ==0) {
         textsearch();
+        dosearch = false;
     } else {
+
         filesearch();
+
     }
+
 }
 
 
@@ -95,12 +105,12 @@ function textsearch() {
                 console.log('no found')
                 dot = '';
                 if(query.length > 50) {
-	            dot = '...' 
-                } 
+                    dot = '...'
+                }
                 kbhtml = '<p> Your search - <b>' + query.substring(0, 50) + dot + '</b> - did not match any documents.</p>' +
                     '<p>Suggestions:</p>' +
                     '<ul><li>Input more logs or upload support bundles.</li><li>Describe your problem with more details.</li><li>Try more keywords.</li></ul>'
-    
+
                 $("#div_search_result").append(kbhtml);
 
             }
@@ -122,6 +132,8 @@ function displayhits() {
 
 
 function listtophits() {
+
+
 
     $.ajax({
         type: "get",
@@ -146,7 +158,7 @@ function listtophits() {
 
                     kbhtml = '<div><h4><a href="' + result["url"] + '">' + result["title"] + '</a></h4><p>' + result["text"] +
                         '</p> <b>' + res[i]['hits']+ '</b> this week </p> ' +
-                    ' <hr></div>'
+                        ' <hr></div>'
 
                     $("#div_top_hit").append(kbhtml);
                 }
@@ -159,11 +171,16 @@ function listtophits() {
 
 
 
+
         }
     })
 }
 
 function filesearch() {
+
+    $('#btn_analyze').empty();
+    $('#btn_analyze').append('<span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span> Processing...');
+
     console.log("handleFileUpload called");
     var query = $('#text_search_input').val();
 
@@ -175,20 +192,88 @@ function filesearch() {
     }
 
     formData.append('text', query);
-
+    var t1 = new Date();
     $.ajax({
         url: url,
         type: "post",
         data: formData,
         processData: false,
         contentType: false,
-        success: function(){
-            //$("#file_upload_result").html('submitted successfully');
+        success: function(res){
+
+
+
+            var t2 = new Date();
+            var dif = t1.getTime() - t2.getTime();
+
+            var Seconds_from_T1_to_T2 = dif / 1000;
+            var td = Math.abs(Seconds_from_T1_to_T2);
+            //display the result
+            $("#div_search_result").empty();
+            res = JSON.parse(res);
+            console.log(res);
+            if(res.length > 0) {
+                //show the total result number
+                $("#div_search_result").append('<h3 class="page-header"> About ' + res.length + ' results  <small>' + td + 'seconds</small> </h3>');
+
+                //show in div div_search_result
+                for (var i = 0; i < res.length; i++) {
+                    var result = res[i];
+                    var title = safe_tags_replace(result["title"]);
+                    //var text = safe_tags_replace(result["text"])
+                    var text = (result["text"]);
+
+                    kbhtml = '<div><h4><a href="' + result["url"] + '">' + title + '</a></h4><p>' + text + '</p> <p><b></b>Similarity:</b>'
+                    var j = 0
+                    for( ; j < result['rank']; j ++) {
+                        kbhtml = kbhtml + '<a href="#"> <span class="glyphicon glyphicon-star"></span> </a>'
+                    }
+
+                    for(; j < 5; j ++) {
+                        kbhtml = kbhtml + '<a href="#"><span class="glyphicon glyphicon-star-empty"></span> </a>'
+                    }
+
+                    //kbhtml = kbhtml + '</p> <p><a class="btn btn-primary" href="#">Comments <span class="glyphicon glyphicon-chevron-right"></span></a></p><hr></div>'
+                    //kbhtml = kbhtml + '</p> <hr></div>'
+
+
+                    $("#div_search_result").append(kbhtml);
+                }
+
+
+            } else {
+                //no result found...
+                console.log('no found')
+                dot = '';
+                if(query.length > 50) {
+                    dot = '...'
+                }
+                kbhtml = '<p> Your search - <b>' + query.substring(0, 50) + dot + '</b> - did not match any documents.</p>' +
+                    '<p>Suggestions:</p>' +
+                    '<ul><li>Input more logs or upload support bundles.</li><li>Describe your problem with more details.</li><li>Try more keywords.</li></ul>'
+
+                $("#div_search_result").append(kbhtml);
+
+            }
+
+
+
+
             console.log("upload successfully")
+            $('#btn_analyze').empty();
+            $('#btn_analyze').append('Analyze!');
+            dosearch = false;
         },
         error:function(){
             //$("#file_upload_result").html('there was an error while submitting');
             console.log("upload failed")
+            $('#btn_analyze').empty();
+            $('#btn_analyze').append('Analyze!');
+            dosearch = false;
         }
+
     });
+
+
+
 }
